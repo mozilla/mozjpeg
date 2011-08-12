33ddbcc5b3180a7bca0f1ab86341c3079b43eccc
DRC <dcommander@users.sourceforge.net>
2011-08-12 19:31:09 +0000

    Oops

--- jversion.h
+++ jversion.h
@@ -32,5 +32,5 @@
 
 #define LJTCOPYRIGHT	"Copyright (C) 1999-2006 MIYASAKA Masaru\n" \
 			"Copyright (C) 2009 Pierre Ossman for Cendio AB\n" \
-			"Copyright (C) 2009-2011 D. R. Commander"
+			"Copyright (C) 2009-2011 D. R. Commander\n" \
 			"Copyright (C) 2009-2011 Nokia Corporation and/or its subsidiary(-ies)"

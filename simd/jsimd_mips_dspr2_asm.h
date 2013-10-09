7b0d18f2398c1ccea2030452d0b0d8702d5ba46e
DRC <dcommander@users.sourceforge.net>
2013-10-09 18:39:44 +0000

    SIMD-accelerated floating point quantize and convsamp routines for MIPS DSPr2
    git-svn-id: svn+ssh://svn.code.sf.net/p/libjpeg-turbo/code/trunk@1058 632fc199-4ca6-4c93-a231-07263d6284db

--- simd/jsimd_mips_dspr2_asm.h
+++ simd/jsimd_mips_dspr2_asm.h
@@ -56,6 +56,39 @@
 #define s8   $30
 #define ra   $31
 
+#define f0   $f0
+#define f1   $f1
+#define f2   $f2
+#define f3   $f3
+#define f4   $f4
+#define f5   $f5
+#define f6   $f6
+#define f7   $f7
+#define f8   $f8
+#define f9   $f9
+#define f10  $f10
+#define f11  $f11
+#define f12  $f12
+#define f13  $f13
+#define f14  $f14
+#define f15  $f15
+#define f16  $f16
+#define f17  $f17
+#define f18  $f18
+#define f19  $f19
+#define f20  $f20
+#define f21  $f21
+#define f22  $f22
+#define f23  $f23
+#define f24  $f24
+#define f25  $f25
+#define f26  $f26
+#define f27  $f27
+#define f28  $f28
+#define f29  $f29
+#define f30  $f30
+#define f31  $f31
+
 /*
  * LEAF_MIPS32R2 - declare leaf routine for MIPS32r2
  */

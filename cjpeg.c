6f168e4f80828e11e73c46cbec21e2a1029ccf25
Frank Bossen <frank@bossentech.com>
2014-03-25 11:48:30 +0100

    Add parameters to cjpeg to tune for different metrics
    -tune-psnr, -tune-ssim and -tune-hvs-psnr are added to cjpeg to control
    the trellis quantization process and optimize output for PSNR, SSIM and
    HVS-PSNR distortion metrics

--- cjpeg.c
+++ cjpeg.c
@@ -465,6 +465,24 @@ parse_switches (j_compress_ptr cinfo, int argc, char **argv,
       cinfo->use_moz_defaults = 2;
       jpeg_set_defaults(cinfo);
       
+    } else if (keymatch(arg, "tune-psnr", 6)) {
+      cinfo->use_flat_quant_tbl = TRUE;
+      cinfo->lambda_log_scale1 = 9.0;
+      cinfo->lambda_log_scale2 = 0.0;
+      jpeg_set_quality(cinfo, 75, TRUE);
+      
+    } else if (keymatch(arg, "tune-ssim", 6)) {
+      cinfo->use_flat_quant_tbl = TRUE;
+      cinfo->lambda_log_scale1 = 11.5;
+      cinfo->lambda_log_scale2 = 13.0;
+      jpeg_set_quality(cinfo, 75, TRUE);
+      
+    } else if (keymatch(arg, "tune-hvs-psnr", 6)) {
+      cinfo->use_flat_quant_tbl = FALSE;
+      cinfo->lambda_log_scale1 = 14.5;
+      cinfo->lambda_log_scale2 = 0.0;
+      jpeg_set_quality(cinfo, 75, TRUE);
+      
     } else {
       usage();			/* bogus switch */
     }

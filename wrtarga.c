90d6c3824bb2dbc98ac04522704c2b4a276cb3f5
DRC <dcommander@users.sourceforge.net>
2014-05-12 09:08:39 +0000

    Document -rgb option in djpeg man page;  "gray-scale"="grayscale"
    git-svn-id: svn+ssh://svn.code.sf.net/p/libjpeg-turbo/code/branches/1.3.x@1293 632fc199-4ca6-4c93-a231-07263d6284db

--- wrtarga.c
+++ wrtarga.c
@@ -73,7 +73,7 @@ write_header (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo, int num_colors)
   targaheader[17] = 0x20;       /* Top-down, non-interlaced */
 
   if (cinfo->out_color_space == JCS_GRAYSCALE) {
-    targaheader[2] = 3;         /* image type = uncompressed gray-scale */
+    targaheader[2] = 3;         /* image type = uncompressed grayscale */
     targaheader[16] = 8;        /* bits per pixel */
   } else {                      /* must be RGB */
     if (num_colors > 0) {

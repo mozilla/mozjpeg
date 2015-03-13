7f0213b1634280eab82f9187b94fe106b1356460
Tristan Matthews <tmatth@videolan.org>
2015-03-13 12:54:51 -0500

    cdjpeg: check for io.h at configure time

--- cdjpeg.c
+++ cdjpeg.c
@@ -15,8 +15,10 @@
 #include <ctype.h>              /* to declare isupper(), tolower() */
 #ifdef USE_SETMODE
 #include <fcntl.h>              /* to declare setmode()'s parameter macros */
+#ifdef HAVE_IO_H
 /* If you have setmode() but not <io.h>, just delete this line: */
 #include <io.h>                 /* to declare setmode() */
+#endif
 #endif
 
 

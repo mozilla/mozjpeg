mozilla/mozjpeg

commit e080e5d90b0a01804071af11613ddef7cac5aec8

Oops.  OS X doesn't define __WORDSIZE unless you include stdint.h, so apparently the Huffman codec hasn't ever been fully accelerated on 64-bit OS X.
git-svn-id: svn+ssh://svn.code.sf.net/p/libjpeg-turbo/code/branches/1.4.x@1541 632fc199-4ca6-4c93-a231-07263d6284db

jchuff.c
@@ -21,6 +21,7 @@
 #include "jpeglib.h"
 #include "jchuff.h"             /* Declarations shared with jcphuff.c */
 #include <limits.h>
+#include <stdint.h>
 
 /*
  * NOTE: If USE_CLZ_INTRINSIC is defined, then clz/bsr instructions will be
@@ -375,6 +376,10 @@ dump_buffer (working_state * state)
     EMIT_BYTE() \
   } \
 }
+
+#if !defined(_WIN32) && !defined(__WORDSIZE)
+#error __WORDSIZE is not defined
+#endif
 
 #if __WORDSIZE==64 || defined(_WIN64)
 

jdhuff.c
@@ -22,6 +22,7 @@
 #include "jdhuff.h"             /* Declarations shared with jdphuff.c */
 #include "jpegcomp.h"
 #include "jstdhuff.c"
+#include <stdint.h>
 
 
 /*
@@ -418,6 +419,10 @@ jpeg_fill_bit_buffer (bitread_working_state * state,
     } \
   } \
 }
+
+#if !defined(_WIN32) && !defined(__WORDSIZE)
+#error __WORDSIZE is not defined
+#endif
 
 #if __WORDSIZE == 64 || defined(_WIN64)
 

commit 5698542e248a84afd2ccc1df0c19de176a3aff03

Restore backward compatibility with MSVC < 2010 (broken by r1541)
git-svn-id: svn+ssh://svn.code.sf.net/p/libjpeg-turbo/code/branches/1.4.x@1543 632fc199-4ca6-4c93-a231-07263d6284db

jchuff.c
@@ -21,7 +21,9 @@
 #include "jpeglib.h"
 #include "jchuff.h"             /* Declarations shared with jcphuff.c */
 #include <limits.h>
+#ifdef HAVE_STDINT_H
 #include <stdint.h>
+#endif
 
 /*
  * NOTE: If USE_CLZ_INTRINSIC is defined, then clz/bsr instructions will be

jconfig.h.in
@@ -32,6 +32,9 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #undef HAVE_STDLIB_H
 
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef HAVE_STDINT_H
+
 /* Define to 1 if the system has the type `unsigned char'. */
 #undef HAVE_UNSIGNED_CHAR
 

jdhuff.c
@@ -22,7 +22,9 @@
 #include "jdhuff.h"             /* Declarations shared with jdphuff.c */
 #include "jpegcomp.h"
 #include "jstdhuff.c"
+#ifdef HAVE_STDINT_H
 #include <stdint.h>
+#endif
 
 
 /*
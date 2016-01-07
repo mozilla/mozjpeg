5a3b4fed4b798ae826bc3bad10147fb3b5b95e9d
DRC <information@libjpeg-turbo.org>
2016-01-06 19:17:54 -0600

    Regression: Allow co-install of 32-bit/64-bit RPMs
    Fix a regression introduced in 1.4.1 that prevented 32-bit and 64-bit
    libjpeg-turbo RPMs from being installed simultaneously on recent Red
    Hat/Fedora distributions.  This was due to the addition of the
    SIZEOF_SIZE_T macro in jconfig.h, which allows the Huffman codec to
    determine the word size at compile time.  Since that macro differs
    between 32-bit and 64-bit builds, this caused a conflict between the
    i386 and x86_64 RPMs (any differing files, other than executables, are
    not allowed when 32-bit and 64-bit RPMs are installed simultaneously.)
    Since the macro is used only internally, it has been moved into
    jconfigint.h.

--- jdhuff.h
+++ jdhuff.h
@@ -4,13 +4,15 @@
  * This file was part of the Independent JPEG Group's software:
  * Copyright (C) 1991-1997, Thomas G. Lane.
  * libjpeg-turbo Modifications:
- * Copyright (C) 2010-2011, D. R. Commander.
+ * Copyright (C) 2010-2011, 2015-2016, D. R. Commander.
  * For conditions of distribution and use, see the accompanying README file.
  *
  * This file contains declarations for Huffman entropy decoding routines
  * that are shared between the sequential decoder (jdhuff.c) and the
  * progressive decoder (jdphuff.c).  No other modules need to see these.
  */
+
+#include "jconfigint.h"
 
 
 /* Derived data constructed for each Huffman table */

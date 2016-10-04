mozilla/mozjpeg

commit dfefba77520ded5c5fd4864e76352a5f3eb23e74

Fix broken build with NDK platforms < android-21
Regression introduced by a09ba29a55b9a43d346421210d94370065eeaf53

Fixes #103

jmemmgr.c
@@ -33,6 +33,7 @@
 #include "jpeglib.h"
 #include "jmemsys.h"            /* import the system-dependent declarations */
 #include <stdint.h>
+#include <limits.h>
 
 #ifndef NO_GETENV
 #ifndef HAVE_STDLIB_H           /* <stdlib.h> should declare getenv() */

commit a0047bdea4d11dfeefb9ea797865b1a2ea0a665e

Fix broken build w/ Visual C++ < 2010
Regression introduced by dfefba77520ded5c5fd4864e76352a5f3eb23e74
(Windows doesn't always have stdint.h.)

jmemmgr.c
@@ -32,7 +32,9 @@
 #include "jinclude.h"
 #include "jpeglib.h"
 #include "jmemsys.h"            /* import the system-dependent declarations */
+#ifndef _WIN32
 #include <stdint.h>
+#endif
 #include <limits.h>
 
 #ifndef NO_GETENV
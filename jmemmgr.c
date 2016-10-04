a0047bdea4d11dfeefb9ea797865b1a2ea0a665e
DRC <information@libjpeg-turbo.org>
2016-10-04 13:25:34 -0500

    Fix broken build w/ Visual C++ < 2010
    Regression introduced by dfefba77520ded5c5fd4864e76352a5f3eb23e74
    (Windows doesn't always have stdint.h.)

--- jmemmgr.c
+++ jmemmgr.c
@@ -32,7 +32,9 @@
 #include "jinclude.h"
 #include "jpeglib.h"
 #include "jmemsys.h"            /* import the system-dependent declarations */
+#ifndef _WIN32
 #include <stdint.h>
+#endif
 #include <limits.h>
 
 #ifndef NO_GETENV

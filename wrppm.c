mozilla/mozjpeg

commit aa7459050d7a50e1d8a99488902d41fbc118a50f

TurboJPEG C API: Add BMP/PPM load/save functions
The main justification for this is to provide new libjpeg-turbo users
with a quick & easy way of developing a complete JPEG
compression/decompression program without requiring them to build
libjpeg-turbo from source (which was necessary in order to use the
project-private bmp API) or to use external libraries.  These new
functions build upon significant enhancements to rdbmp.c, wrbmp.c,
rdppm.c, and wrppm.c which allow those engines to convert directly
between the native pixel format of the file and a pixel format
("colorspace" in libjpeg parlance) specified by the calling program.
rdbmp.c and wrbmp.c have also been modified such that the calling
program can choose to read or write image rows in the native (bottom-up)
order of the file format, thus eliminating the need to use an inversion
array.  tjLoadImage() and tjSaveImage() leverage these new underlying
features in order to significantly improve upon the performance of the
old bmp API.

Because these new functions cannot work without the libjpeg-turbo
colorspace extensions, the libjpeg-compatible code in turbojpeg.c has
been removed.  That code was only there to serve as an example of how
to use the TurboJPEG API on top of libjpeg, but more specific, buildable
examples now exist in the https://github.com/libjpeg-turbo/ijg
repository.

wrppm.c
@@ -20,6 +20,7 @@
  */
 
 #include "cdjpeg.h"             /* Common decls for cjpeg/djpeg applications */
+#include "cmyk.h"
 
 #ifdef PPM_SUPPORTED
 
@@ -107,12 +108,73 @@ copy_pixel_rows (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
   ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
   register char *bufferptr;
   register JSAMPROW ptr;
+#if BITS_IN_JSAMPLE != 8 || (!defined(HAVE_UNSIGNED_CHAR) && !defined(__CHAR_UNSIGNED__))
+  register JDIMENSION col;
+#endif
+
+  ptr = dest->pub.buffer[0];
+  bufferptr = dest->iobuffer;
+#if BITS_IN_JSAMPLE == 8 && (defined(HAVE_UNSIGNED_CHAR) || defined(__CHAR_UNSIGNED__))
+  MEMCOPY(bufferptr, ptr, dest->samples_per_row);
+#else
+  for (col = dest->samples_per_row; col > 0; col--) {
+    PUTPPMSAMPLE(bufferptr, GETJSAMPLE(*ptr++));
+  }
+#endif
+  (void) JFWRITE(dest->pub.output_file, dest->iobuffer, dest->buffer_width);
+}
+
+
+/*
+ * Convert extended RGB to RGB.
+ */
+
+METHODDEF(void)
+put_rgb (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
+         JDIMENSION rows_supplied)
+{
+  ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
+  register char *bufferptr;
+  register JSAMPROW ptr;
+  register JDIMENSION col;
+  register int rindex = rgb_red[cinfo->out_color_space];
+  register int gindex = rgb_green[cinfo->out_color_space];
+  register int bindex = rgb_blue[cinfo->out_color_space];
+  register int ps = rgb_pixelsize[cinfo->out_color_space];
+
+  ptr = dest->pub.buffer[0];
+  bufferptr = dest->iobuffer;
+  for (col = cinfo->output_width; col > 0; col--) {
+    PUTPPMSAMPLE(bufferptr, ptr[rindex]);
+    PUTPPMSAMPLE(bufferptr, ptr[gindex]);
+    PUTPPMSAMPLE(bufferptr, ptr[bindex]);
+    ptr += ps;
+  }
+  (void) JFWRITE(dest->pub.output_file, dest->iobuffer, dest->buffer_width);
+}
+
+
+/*
+ * Convert CMYK to RGB.
+ */
+
+METHODDEF(void)
+put_cmyk (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo,
+          JDIMENSION rows_supplied)
+{
+  ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
+  register char *bufferptr;
+  register JSAMPROW ptr;
   register JDIMENSION col;
 
   ptr = dest->pub.buffer[0];
   bufferptr = dest->iobuffer;
-  for (col = dest->samples_per_row; col > 0; col--) {
-    PUTPPMSAMPLE(bufferptr, GETJSAMPLE(*ptr++));
+  for (col = cinfo->output_width; col > 0; col--) {
+    JSAMPLE r, g, b, c = *ptr++, m = *ptr++, y = *ptr++, k = *ptr++;
+    cmyk_to_rgb(c, m, y, k, &r, &g, &b);
+    PUTPPMSAMPLE(bufferptr, r);
+    PUTPPMSAMPLE(bufferptr, g);
+    PUTPPMSAMPLE(bufferptr, b);
   }
   (void) JFWRITE(dest->pub.output_file, dest->iobuffer, dest->buffer_width);
 }
@@ -185,6 +247,17 @@ start_output_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
             PPM_MAXVAL);
     break;
   case JCS_RGB:
+  case JCS_EXT_RGB:
+  case JCS_EXT_RGBX:
+  case JCS_EXT_BGR:
+  case JCS_EXT_BGRX:
+  case JCS_EXT_XBGR:
+  case JCS_EXT_XRGB:
+  case JCS_EXT_RGBA:
+  case JCS_EXT_BGRA:
+  case JCS_EXT_ABGR:
+  case JCS_EXT_ARGB:
+  case JCS_CMYK:
     /* emit header for raw PPM format */
     fprintf(dest->pub.output_file, "P6\n%ld %ld\n%d\n",
             (long) cinfo->output_width, (long) cinfo->output_height,
@@ -219,7 +292,10 @@ calc_buffer_dimensions_ppm (j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
 {
   ppm_dest_ptr dest = (ppm_dest_ptr) dinfo;
 
-  dest->samples_per_row = cinfo->output_width * cinfo->out_color_components;
+  if (cinfo->out_color_space == JCS_GRAYSCALE)
+    dest->samples_per_row = cinfo->output_width * cinfo->out_color_components;
+  else
+    dest->samples_per_row = cinfo->output_width * 3;
   dest->buffer_width = dest->samples_per_row * (BYTESPERSAMPLE * sizeof(char));
 }
 
@@ -250,7 +326,12 @@ jinit_write_ppm (j_decompress_ptr cinfo)
     ((j_common_ptr) cinfo, JPOOL_IMAGE, dest->buffer_width);
 
   if (cinfo->quantize_colors || BITS_IN_JSAMPLE != 8 ||
-      sizeof(JSAMPLE) != sizeof(char)) {
+      sizeof(JSAMPLE) != sizeof(char) ||
+      (cinfo->out_color_space != JCS_EXT_RGB
+#if RGB_RED == 0 && RGB_GREEN == 1 && RGB_BLUE == 2 && RGB_PIXELSIZE == 3
+       && cinfo->out_color_space != JCS_RGB
+#endif
+      )) {
     /* When quantizing, we need an output buffer for colormap indexes
      * that's separate from the physical I/O buffer.  We also need a
      * separate buffer if pixel format translation must take place.
@@ -259,7 +340,11 @@ jinit_write_ppm (j_decompress_ptr cinfo)
       ((j_common_ptr) cinfo, JPOOL_IMAGE,
        cinfo->output_width * cinfo->output_components, (JDIMENSION) 1);
     dest->pub.buffer_height = 1;
-    if (! cinfo->quantize_colors)
+    if (IsExtRGB(cinfo->out_color_space))
+      dest->pub.put_pixel_rows = put_rgb;
+    else if (cinfo->out_color_space == JCS_CMYK)
+      dest->pub.put_pixel_rows = put_cmyk;
+    else if (! cinfo->quantize_colors)
       dest->pub.put_pixel_rows = copy_pixel_rows;
     else if (cinfo->out_color_space == JCS_GRAYSCALE)
       dest->pub.put_pixel_rows = put_demapped_gray;

CMakeLists.txt
@@ -541,7 +541,8 @@ endif()
 if(WITH_TURBOJPEG)
   if(ENABLE_SHARED)
     set(TURBOJPEG_SOURCES ${JPEG_SOURCES} $<TARGET_OBJECTS:simd> ${SIMD_OBJS}
-      turbojpeg.c transupp.c jdatadst-tj.c jdatasrc-tj.c)
+      turbojpeg.c transupp.c jdatadst-tj.c jdatasrc-tj.c rdbmp.c rdppm.c
+      wrbmp.c wrppm.c)
     set(TJMAPFILE ${CMAKE_CURRENT_SOURCE_DIR}/turbojpeg-mapfile)
     if(WITH_JAVA)
       set(TURBOJPEG_SOURCES ${TURBOJPEG_SOURCES} turbojpeg-jni.c)
@@ -549,6 +550,8 @@ if(WITH_TURBOJPEG)
       set(TJMAPFILE ${CMAKE_CURRENT_SOURCE_DIR}/turbojpeg-mapfile.jni)
     endif()
     add_library(turbojpeg SHARED ${TURBOJPEG_SOURCES})
+    set_property(TARGET turbojpeg PROPERTY COMPILE_FLAGS
+      "-DBMP_SUPPORTED -DPPM_SUPPORTED")
     if(WIN32)
       set_target_properties(turbojpeg PROPERTIES DEFINE_SYMBOL DLLDEFINE)
     endif()
@@ -565,37 +568,35 @@ if(WITH_TURBOJPEG)
         LINK_FLAGS "${TJMAPFLAG}${TJMAPFILE}")
     endif()
 
-    add_executable(tjunittest tjunittest.c tjutil.c)
+    add_executable(tjunittest tjunittest.c tjutil.c md5/md5.c md5/md5hl.c)
     target_link_libraries(tjunittest turbojpeg)
 
-    add_executable(tjbench tjbench.c bmp.c tjutil.c rdbmp.c rdppm.c wrbmp.c
-      wrppm.c)
-    target_link_libraries(tjbench turbojpeg jpeg)
+    add_executable(tjbench tjbench.c tjutil.c)
+    target_link_libraries(tjbench turbojpeg)
     if(UNIX)
       target_link_libraries(tjbench m)
     endif()
-    set_property(TARGET tjbench PROPERTY COMPILE_FLAGS
-      "-DBMP_SUPPORTED -DPPM_SUPPORTED")
   endif()
 
   if(ENABLE_STATIC)
     add_library(turbojpeg-static STATIC ${JPEG_SOURCES} $<TARGET_OBJECTS:simd>
-      ${SIMD_OBJS} turbojpeg.c transupp.c jdatadst-tj.c jdatasrc-tj.c)
+      ${SIMD_OBJS} turbojpeg.c transupp.c jdatadst-tj.c jdatasrc-tj.c rdbmp.c
+      rdppm.c wrbmp.c wrppm.c)
+    set_property(TARGET turbojpeg-static PROPERTY COMPILE_FLAGS
+      "-DBMP_SUPPORTED -DPPM_SUPPORTED")
     if(NOT MSVC)
       set_target_properties(turbojpeg-static PROPERTIES OUTPUT_NAME turbojpeg)
     endif()
 
-    add_executable(tjunittest-static tjunittest.c tjutil.c)
+    add_executable(tjunittest-static tjunittest.c tjutil.c md5/md5.c
+      md5/md5hl.c)
     target_link_libraries(tjunittest-static turbojpeg-static)
 
-    add_executable(tjbench-static tjbench.c bmp.c tjutil.c rdbmp.c rdppm.c
-      wrbmp.c wrppm.c)
-    target_link_libraries(tjbench-static turbojpeg-static jpeg-static)
+    add_executable(tjbench-static tjbench.c tjutil.c)
+    target_link_libraries(tjbench-static turbojpeg-static)
     if(UNIX)
       target_link_libraries(tjbench-static m)
     endif()
-    set_property(TARGET tjbench-static PROPERTY COMPILE_FLAGS
-      "-DBMP_SUPPORTED -DPPM_SUPPORTED")
   endif()
 endif()
 
@@ -864,6 +865,7 @@ foreach(libtype ${TEST_LIBTYPES})
     add_test(tjunittest-${libtype}-yuv tjunittest${suffix} -yuv)
     add_test(tjunittest-${libtype}-yuv-alloc tjunittest${suffix} -yuv -alloc)
     add_test(tjunittest-${libtype}-yuv-nopad tjunittest${suffix} -yuv -noyuvpad)
+    add_test(tjunittest-${libtype}-bmp tjunittest${suffix} -bmp)
 
     set(MD5_PPM_GRAY_TILE 89d3ca21213d9d864b50b4e4e7de4ca6)
     set(MD5_PPM_420_8x8_TILE 847fceab15c5b7b911cb986cf0f71de3)

bmp.c
@@ -1,341 +1,0 @@
-/*
- * Copyright (C)2011, 2015 D. R. Commander.  All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * - Neither the name of the libjpeg-turbo Project nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS",
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <stdio.h>
-#include <string.h>
-#include <setjmp.h>
-#include <errno.h>
-#include "cdjpeg.h"
-#include <jpeglib.h>
-#include <jpegint.h>
-#include "tjutil.h"
-#include "bmp.h"
-
-
-/* This duplicates the functionality of the VirtualGL bitmap library using
-   the components from cjpeg and djpeg */
-
-
-/* Error handling (based on example in example.c) */
-
-static char errStr[JMSG_LENGTH_MAX]="No error";
-
-struct my_error_mgr
-{
-	struct jpeg_error_mgr pub;
-	jmp_buf setjmp_buffer;
-};
-typedef struct my_error_mgr *my_error_ptr;
-
-static void my_error_exit(j_common_ptr cinfo)
-{
-	my_error_ptr myerr=(my_error_ptr)cinfo->err;
-	(*cinfo->err->output_message)(cinfo);
-	longjmp(myerr->setjmp_buffer, 1);
-}
-
-/* Based on output_message() in jerror.c */
-
-static void my_output_message(j_common_ptr cinfo)
-{
-	(*cinfo->err->format_message)(cinfo, errStr);
-}
-
-#define _throw(m) {snprintf(errStr, JMSG_LENGTH_MAX, "%s", m);  \
-	retval=-1;  goto bailout;}
-#define _throwunix(m) {snprintf(errStr, JMSG_LENGTH_MAX, "%s\n%s", m,  \
-	strerror(errno));  retval=-1;  goto bailout;}
-
-
-static void pixelconvert(unsigned char *srcbuf, int srcpf, int srcbottomup,
-	unsigned char *dstbuf, int dstpf, int dstbottomup, int w, int h)
-{
-	unsigned char *srcrowptr=srcbuf, *srccolptr;
-	int srcps=tjPixelSize[srcpf];
-	int srcstride=srcbottomup? -w*srcps:w*srcps;
-	unsigned char *dstrowptr=dstbuf, *dstcolptr;
-	int dstps=tjPixelSize[dstpf];
-	int dststride=dstbottomup? -w*dstps:w*dstps;
-	int row, col;
-
-	if(srcbottomup) srcrowptr=&srcbuf[w*srcps*(h-1)];
-	if(dstbottomup) dstrowptr=&dstbuf[w*dstps*(h-1)];
-
-	/* NOTE: These quick & dirty CMYK<->RGB conversion routines are for testing
-	   purposes only.  Properly converting between CMYK and RGB requires a color
-	   management system. */
-
-	if(dstpf==TJPF_CMYK)
-	{
-		for(row=0; row<h; row++, srcrowptr+=srcstride, dstrowptr+=dststride)
-		{
-			for(col=0, srccolptr=srcrowptr, dstcolptr=dstrowptr;
-				col<w; col++, srccolptr+=srcps)
-			{
-				double c=1.0-((double)(srccolptr[tjRedOffset[srcpf]])/255.);
-				double m=1.0-((double)(srccolptr[tjGreenOffset[srcpf]])/255.);
-				double y=1.0-((double)(srccolptr[tjBlueOffset[srcpf]])/255.);
-				double k=min(min(c,m),min(y,1.0));
-				if(k==1.0) c=m=y=0.0;
-				else
-				{
-					c=(c-k)/(1.0-k);
-					m=(m-k)/(1.0-k);
-					y=(y-k)/(1.0-k);
-				}
-				if(c>1.0) c=1.0;
-				if(c<0.) c=0.;
-				if(m>1.0) m=1.0;
-				if(m<0.) m=0.;
-				if(y>1.0) y=1.0;
-				if(y<0.) y=0.;
-				if(k>1.0) k=1.0;
-				if(k<0.) k=0.;
-				*dstcolptr++=(unsigned char)(255.0-c*255.0+0.5);
-				*dstcolptr++=(unsigned char)(255.0-m*255.0+0.5);
-				*dstcolptr++=(unsigned char)(255.0-y*255.0+0.5);
-				*dstcolptr++=(unsigned char)(255.0-k*255.0+0.5);
-			}
-		}
-	}
-	else if(srcpf==TJPF_CMYK)
-	{
-		for(row=0; row<h; row++, srcrowptr+=srcstride, dstrowptr+=dststride)
-		{
-			for(col=0, srccolptr=srcrowptr, dstcolptr=dstrowptr;
-				col<w; col++, dstcolptr+=dstps)
-			{
-				double c=(double)(*srccolptr++);
-				double m=(double)(*srccolptr++);
-				double y=(double)(*srccolptr++);
-				double k=(double)(*srccolptr++);
-				double r=c*k/255.;
-				double g=m*k/255.;
-				double b=y*k/255.;
-				if(r>255.0) r=255.0;
-				if(r<0.) r=0.;
-				if(g>255.0) g=255.0;
-				if(g<0.) g=0.;
-				if(b>255.0) b=255.0;
-				if(b<0.) b=0.;
-				dstcolptr[tjRedOffset[dstpf]]=(unsigned char)(r+0.5);
-				dstcolptr[tjGreenOffset[dstpf]]=(unsigned char)(g+0.5);
-				dstcolptr[tjBlueOffset[dstpf]]=(unsigned char)(b+0.5);
-			}
-		}
-	}
-	else
-	{
-		for(row=0; row<h; row++, srcrowptr+=srcstride, dstrowptr+=dststride)
-		{
-			for(col=0, srccolptr=srcrowptr, dstcolptr=dstrowptr;
-				col<w; col++, srccolptr+=srcps, dstcolptr+=dstps)
-			{
-				dstcolptr[tjRedOffset[dstpf]]=srccolptr[tjRedOffset[srcpf]];
-				dstcolptr[tjGreenOffset[dstpf]]=srccolptr[tjGreenOffset[srcpf]];
-				dstcolptr[tjBlueOffset[dstpf]]=srccolptr[tjBlueOffset[srcpf]];
-			}
-		}
-	}
-}
-
-
-int loadbmp(char *filename, unsigned char **buf, int *w, int *h,
-	int dstpf, int bottomup)
-{
-	int retval=0, dstps, srcpf, tempc;
-	struct jpeg_compress_struct cinfo;
-	struct my_error_mgr jerr;
-	cjpeg_source_ptr src;
-	FILE *file=NULL;
-
-	memset(&cinfo, 0, sizeof(struct jpeg_compress_struct));
-
-	if(!filename || !buf || !w || !h || dstpf<0 || dstpf>=TJ_NUMPF)
-		_throw("loadbmp(): Invalid argument");
-
-	if((file=fopen(filename, "rb"))==NULL)
-		_throwunix("loadbmp(): Cannot open input file");
-
-	cinfo.err=jpeg_std_error(&jerr.pub);
-	jerr.pub.error_exit=my_error_exit;
-	jerr.pub.output_message=my_output_message;
-
-	if(setjmp(jerr.setjmp_buffer))
-	{
-		/* If we get here, the JPEG code has signaled an error. */
-		retval=-1;  goto bailout;
-	}
-
-	jpeg_create_compress(&cinfo);
-	if((tempc=getc(file))<0 || ungetc(tempc, file)==EOF)
-		_throwunix("loadbmp(): Could not read input file")
-	else if(tempc==EOF) _throw("loadbmp(): Input file contains no data");
-
-	if(tempc=='B')
-	{
-		if((src=jinit_read_bmp(&cinfo))==NULL)
-			_throw("loadbmp(): Could not initialize bitmap loader");
-	}
-	else if(tempc=='P')
-	{
-		if((src=jinit_read_ppm(&cinfo))==NULL)
-			_throw("loadbmp(): Could not initialize bitmap loader");
-	}
-	else _throw("loadbmp(): Unsupported file type");
-
-	src->input_file=file;
-	(*src->start_input)(&cinfo, src);
-	(*cinfo.mem->realize_virt_arrays)((j_common_ptr)&cinfo);
-
-	*w=cinfo.image_width;  *h=cinfo.image_height;
-
-	if(cinfo.input_components==1 && cinfo.in_color_space==JCS_RGB)
-		srcpf=TJPF_GRAY;
-	else srcpf=TJPF_RGB;
-
-	dstps=tjPixelSize[dstpf];
-	if((*buf=(unsigned char *)malloc((*w)*(*h)*dstps))==NULL)
-		_throw("loadbmp(): Memory allocation failure");
-
-	while(cinfo.next_scanline<cinfo.image_height)
-	{
-		int i, nlines=(*src->get_pixel_rows)(&cinfo, src);
-		for(i=0; i<nlines; i++)
-		{
-			unsigned char *outbuf;  int row;
-			row=cinfo.next_scanline+i;
-			if(bottomup) outbuf=&(*buf)[((*h)-row-1)*(*w)*dstps];
-			else outbuf=&(*buf)[row*(*w)*dstps];
-			pixelconvert(src->buffer[i], srcpf, 0, outbuf, dstpf, bottomup, *w,
-				nlines);
-		}
-		cinfo.next_scanline+=nlines;
-	}
-
-	(*src->finish_input)(&cinfo, src);
-
-	bailout:
-	jpeg_destroy_compress(&cinfo);
-	if(file) fclose(file);
-	if(retval<0 && buf && *buf) {free(*buf);  *buf=NULL;}
-	return retval;
-}
-
-
-int savebmp(char *filename, unsigned char *buf, int w, int h, int srcpf,
-	int bottomup)
-{
-	int retval=0, srcps, dstpf;
-	struct jpeg_decompress_struct dinfo;
-	struct my_error_mgr jerr;
-	djpeg_dest_ptr dst;
-	FILE *file=NULL;
-	char *ptr=NULL;
-
-	memset(&dinfo, 0, sizeof(struct jpeg_decompress_struct));
-
-	if(!filename || !buf || w<1 || h<1 || srcpf<0 || srcpf>=TJ_NUMPF)
-		_throw("savebmp(): Invalid argument");
-
-	if((file=fopen(filename, "wb"))==NULL)
-		_throwunix("savebmp(): Cannot open output file");
-
-	dinfo.err=jpeg_std_error(&jerr.pub);
-	jerr.pub.error_exit=my_error_exit;
-	jerr.pub.output_message=my_output_message;
-
-	if(setjmp(jerr.setjmp_buffer))
-	{
-		/* If we get here, the JPEG code has signaled an error. */
-		retval=-1;  goto bailout;
-	}
-
-	jpeg_create_decompress(&dinfo);
-	if(srcpf==TJPF_GRAY)
-	{
-		dinfo.out_color_components=dinfo.output_components=1;
-		dinfo.out_color_space=JCS_GRAYSCALE;
-	}
-	else
-	{
-		dinfo.out_color_components=dinfo.output_components=3;
-		dinfo.out_color_space=JCS_RGB;
-	}
-	dinfo.image_width=w;  dinfo.image_height=h;
-	dinfo.global_state=DSTATE_READY;
-	dinfo.scale_num=dinfo.scale_denom=1;
-
-	ptr=strrchr(filename, '.');
-	if(ptr && !strcasecmp(ptr, ".bmp"))
-	{
-		if((dst=jinit_write_bmp(&dinfo, 0))==NULL)
-			_throw("savebmp(): Could not initialize bitmap writer");
-	}
-	else
-	{
-		if((dst=jinit_write_ppm(&dinfo))==NULL)
-			_throw("savebmp(): Could not initialize PPM writer");
-	}
-
-	dst->output_file=file;
-	(*dst->start_output)(&dinfo, dst);
-	(*dinfo.mem->realize_virt_arrays)((j_common_ptr)&dinfo);
-
-	if(srcpf==TJPF_GRAY) dstpf=srcpf;
-	else dstpf=TJPF_RGB;
-	srcps=tjPixelSize[srcpf];
-
-	while(dinfo.output_scanline<dinfo.output_height)
-	{
-		int i, nlines=dst->buffer_height;
-		for(i=0; i<nlines; i++)
-		{
-			unsigned char *inbuf;  int row;
-			row=dinfo.output_scanline+i;
-			if(bottomup) inbuf=&buf[(h-row-1)*w*srcps];
-			else inbuf=&buf[row*w*srcps];
-			pixelconvert(inbuf, srcpf, bottomup, dst->buffer[i], dstpf, 0, w,
-				nlines);
-		}
-		(*dst->put_pixel_rows)(&dinfo, dst, nlines);
-		dinfo.output_scanline+=nlines;
-	}
-
-	(*dst->finish_output)(&dinfo, dst);
-
-	bailout:
-	jpeg_destroy_decompress(&dinfo);
-	if(file) fclose(file);
-	return retval;
-}
-
-const char *bmpgeterr(void)
-{
-	return errStr;
-}

bmp.h
@@ -1,42 +1,0 @@
-/*
- * Copyright (C)2011 D. R. Commander.  All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * - Neither the name of the libjpeg-turbo Project nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS",
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifndef __BMP_H__
-#define __BMP_H__
-
-#include "./turbojpeg.h"
-
-int loadbmp(char *filename, unsigned char **buf, int *w, int *h, int pf,
-	int bottomup);
-
-int savebmp(char *filename, unsigned char *buf, int w, int h, int pf,
-	int bottomup);
-
-const char *bmpgeterr(void);
-
-#endif

cdjpeg.h
@@ -96,9 +96,11 @@ typedef struct cdjpeg_progress_mgr *cd_progress_ptr;
 
 /* Module selection routines for I/O modules. */
 
-EXTERN(cjpeg_source_ptr) jinit_read_bmp (j_compress_ptr cinfo);
+EXTERN(cjpeg_source_ptr) jinit_read_bmp (j_compress_ptr cinfo,
+                                         boolean use_inversion_array);
 EXTERN(djpeg_dest_ptr) jinit_write_bmp (j_decompress_ptr cinfo,
-                                        boolean is_os2);
+                                        boolean is_os2,
+                                        boolean use_inversion_array);
 EXTERN(cjpeg_source_ptr) jinit_read_gif (j_compress_ptr cinfo);
 EXTERN(djpeg_dest_ptr) jinit_write_gif (j_decompress_ptr cinfo);
 EXTERN(cjpeg_source_ptr) jinit_read_ppm (j_compress_ptr cinfo);
@@ -151,3 +153,10 @@ EXTERN(FILE *) write_stdout (void);
 #ifndef EXIT_WARNING
 #define EXIT_WARNING  2
 #endif
+
+#define RGB2GRAY(r, g, b)  \
+  (JSAMPLE)((double)(r) * 0.299 + (double)(g) * 0.587 +  \
+            (double)(b) * 0.114 + 0.5)
+
+#define IsExtRGB(cs)  \
+  (cs == JCS_RGB || (cs >= JCS_EXT_RGB && cs <= JCS_EXT_ARGB))

cjpeg.c
@@ -107,7 +107,7 @@ select_file_type (j_compress_ptr cinfo, FILE *infile)
   switch (c) {
 #ifdef BMP_SUPPORTED
   case 'B':
-    return jinit_read_bmp(cinfo);
+    return jinit_read_bmp(cinfo, TRUE);
 #endif
 #ifdef GIF_SUPPORTED
   case 'G':

cmyk.h
@@ -1,0 +1,64 @@
+/*
+ * cmyk.h
+ *
+ * Copyright (C) 2017, D. R. Commander.
+ * For conditions of distribution and use, see the accompanying README.ijg
+ * file.
+ *
+ * This file contains convenience functions for performing quick & dirty
+ * CMYK<->RGB conversion.  This algorithm is suitable for testing purposes
+ * only.  Properly converting between CMYK and RGB requires a color management
+ * system.
+ */
+
+#include <jpeglib.h>
+#include "jconfigint.h"
+
+#ifndef CMYK_H
+#define CMYK_H
+
+
+#ifndef min
+ #define min(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
+
+/* Fully reversible */
+
+INLINE
+LOCAL(void)
+rgb_to_cmyk(JSAMPLE r, JSAMPLE g, JSAMPLE b, JSAMPLE *c, JSAMPLE *m,
+            JSAMPLE *y, JSAMPLE *k)
+{
+  double ctmp = 1.0 - ((double)r / 255.0);
+  double mtmp = 1.0 - ((double)g / 255.0);
+  double ytmp = 1.0 - ((double)b / 255.0);
+  double ktmp = min(min(ctmp, mtmp), ytmp);
+
+  if (ktmp == 1.0) ctmp = mtmp = ytmp = 0.0;
+  else {
+    ctmp = (ctmp - ktmp) / (1.0 - ktmp);
+    mtmp = (mtmp - ktmp) / (1.0 - ktmp);
+    ytmp = (ytmp - ktmp) / (1.0 - ktmp);
+  }
+  *c = (JSAMPLE)(255.0 - ctmp * 255.0 + 0.5);
+  *m = (JSAMPLE)(255.0 - mtmp * 255.0 + 0.5);
+  *y = (JSAMPLE)(255.0 - ytmp * 255.0 + 0.5);
+  *k = (JSAMPLE)(255.0 - ktmp * 255.0 + 0.5);
+}
+
+
+/* Fully reversible only for C/M/Y/K values generated with rgb_to_cmyk() */
+
+INLINE
+LOCAL(void)
+cmyk_to_rgb(JSAMPLE c, JSAMPLE m, JSAMPLE y, JSAMPLE k, JSAMPLE *r, JSAMPLE *g,
+            JSAMPLE *b)
+{
+  *r = (JSAMPLE)((double)c * (double)k / 255.0 + 0.5);
+  *g = (JSAMPLE)((double)m * (double)k / 255.0 + 0.5);
+  *b = (JSAMPLE)((double)y * (double)k / 255.0 + 0.5);
+}
+
+
+#endif /* CMYK_H */

djpeg.c
@@ -642,10 +642,10 @@ main (int argc, char **argv)
   switch (requested_fmt) {
 #ifdef BMP_SUPPORTED
   case FMT_BMP:
-    dest_mgr = jinit_write_bmp(&cinfo, FALSE);
+    dest_mgr = jinit_write_bmp(&cinfo, FALSE, TRUE);
     break;
   case FMT_OS2:
-    dest_mgr = jinit_write_bmp(&cinfo, TRUE);
+    dest_mgr = jinit_write_bmp(&cinfo, TRUE, TRUE);
     break;
 #endif
 #ifdef GIF_SUPPORTED

commit 1ee87a9e2ea14b84201c28ea2df09e2319486835

djpeg: Fix PPM output regression w/ color quant.
Regression caused by aa7459050d7a50e1d8a99488902d41fbc118a50f

Fix based on:
https://github.com/sinic/libjpeg-turbo/commit/03fbacb8ebf1fffc3f2d2db26ddf4db8b1f6aa7b

Closes #310

wrppm.c
@@ -5,7 +5,7 @@
  * Copyright (C) 1991-1996, Thomas G. Lane.
  * Modified 2009 by Guido Vollbeding.
  * libjpeg-turbo Modifications:
- * Copyright (C) 2017, D. R. Commander.
+ * Copyright (C) 2017, 2019, D. R. Commander.
  * For conditions of distribution and use, see the accompanying README.ijg
  * file.
  *
@@ -256,6 +256,8 @@ start_output_ppm(j_decompress_ptr cinfo, djpeg_dest_ptr dinfo)
   case JCS_EXT_ABGR:
   case JCS_EXT_ARGB:
   case JCS_CMYK:
+    if (!IsExtRGB(cinfo->out_color_space) && cinfo->quantize_colors)
+      ERREXIT(cinfo, JERR_PPM_COLORSPACE);
     /* emit header for raw PPM format */
     fprintf(dest->pub.output_file, "P6\n%ld %ld\n%d\n",
             (long)cinfo->output_width, (long)cinfo->output_height, PPM_MAXVAL);
@@ -337,13 +339,14 @@ jinit_write_ppm(j_decompress_ptr cinfo)
       ((j_common_ptr)cinfo, JPOOL_IMAGE,
        cinfo->output_width * cinfo->output_components, (JDIMENSION)1);
     dest->pub.buffer_height = 1;
-    if (IsExtRGB(cinfo->out_color_space))
-      dest->pub.put_pixel_rows = put_rgb;
-    else if (cinfo->out_color_space == JCS_CMYK)
-      dest->pub.put_pixel_rows = put_cmyk;
-    else if (!cinfo->quantize_colors)
-      dest->pub.put_pixel_rows = copy_pixel_rows;
-    else if (cinfo->out_color_space == JCS_GRAYSCALE)
+    if (!cinfo->quantize_colors) {
+      if (IsExtRGB(cinfo->out_color_space))
+        dest->pub.put_pixel_rows = put_rgb;
+      else if (cinfo->out_color_space == JCS_CMYK)
+        dest->pub.put_pixel_rows = put_cmyk;
+      else
+        dest->pub.put_pixel_rows = copy_pixel_rows;
+    } else if (cinfo->out_color_space == JCS_GRAYSCALE)
       dest->pub.put_pixel_rows = put_demapped_gray;
     else
       dest->pub.put_pixel_rows = put_demapped_rgb;
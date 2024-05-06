7fa4b5b762c9a99b46b0b7838f5fd55071b92ea5
DRC <information@libjpeg-turbo.org>
2024-05-06 17:28:07 -0400

    jerror.c: Silence MSan uninitialized value warning
    If an error manager instance is passed to jpeg_std_error(), then its
    format_message() method will point to the format_message() function in
    jerror.c.  The format_message() function passes all eight values from
    the jpeg_error_mgr::msg_parm.i[] array as arguments to
    snprintf()/_snprintf_s(), even if the format string doesn't use all of
    those values.  Subsequently invoking one of the ERREXIT[1-6]() macros
    will leave the unused values uninitialized, and if the
    -fsanitize-memory-param-retval option (introduced in Clang 14) is
    enabled (which it is by default in Clang 16 and later), then MSan will
    complain when the format_message() function tries to pass the
    uninitialized-but-unused values as function arguments.
    
    This commit modifies jpeg_std_error() so that it zeroes out the error
    manager instance passed to it, thus working around the warning as well
    as simplifying the code.
    
    Closes #761

--- jerror.c
+++ jerror.c
@@ -229,23 +229,17 @@ reset_error_mgr(j_common_ptr cinfo)
 GLOBAL(struct jpeg_error_mgr *)
 jpeg_std_error(struct jpeg_error_mgr *err)
 {
+  memset(err, 0, sizeof(struct jpeg_error_mgr));
+
   err->error_exit = error_exit;
   err->emit_message = emit_message;
   err->output_message = output_message;
   err->format_message = format_message;
   err->reset_error_mgr = reset_error_mgr;
 
-  err->trace_level = 0;         /* default = no tracing */
-  err->num_warnings = 0;        /* no warnings emitted yet */
-  err->msg_code = 0;            /* may be useful as a flag for "no error" */
-
   /* Initialize message table pointers */
   err->jpeg_message_table = jpeg_std_message_table;
   err->last_jpeg_message = (int)JMSG_LASTMSGCODE - 1;
-
-  err->addon_message_table = NULL;
-  err->first_addon_message = 0; /* for safety */
-  err->last_addon_message = 0;
 
   return err;
 }
